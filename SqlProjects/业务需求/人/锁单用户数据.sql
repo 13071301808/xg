070a6135aa76108881a2a76f3e55edc664b9d414
xg <123@qq.com>
2025-04-29 11:57:02 +0800

    测试vscode

--- SqlProjects/业务需求/人/锁单用户数据.sql
+++ SqlProjects/业务需求/人/锁单用户数据.sql
@@ -1,13 +1,22 @@
--- 口径：https://yb7ao262ru.feishu.cn/wiki/JHQfwOYYcisyCikOlMJcXmWpndd
 select 
     pay.special_date
     ,pay.user_id
     ,pay.order_sn
     ,pay.order_status
-    ,sum(real_buy_amount) real_buy_amount
-    ,sum(real_buy_num) real_buy_num
-    ,sum(cancel_buy_num) cancel_buy_num
-    ,sum(cancel_buy_amount) cancel_buy_amount
+    ,case 
+        when suo.user_id is not null and suo.order_sn is not null and suo.special_date is not null 
+        then '是'
+        else '否' 
+    end as is_suo_windows
+    ,case 
+        when qiyu.user_id is not null and qiyu.special_date is not null 
+        then '是' 
+        else '否' 
+    end as is_enter_qiyu
+    ,sum(pay.real_buy_amount) real_buy_amount
+    ,sum(pay.real_buy_num) real_buy_num
+    ,sum(pay.cancel_buy_num) cancel_buy_num
+    ,sum(pay.cancel_buy_amount) cancel_buy_amount
 from (
     select 
         to_char(dateadd(pay.add_time,-7,'hh'),'yyyymmdd') as special_date
@@ -23,9 +32,11 @@ from (
     left join yishou_data.all_fmys_order_cancel_record cr on pay.order_id = cr.order_id and cancel_type = 1
     where pay.dt between '20240826' and '20250426'
     and to_char(dateadd(pay.add_time,-7,'hh'),'yyyymmdd') between '20240826' and '20250426'
+    and pay.pay_status = 1
     group by 1,2,3,4,5
 ) pay
 left join (
+    -- 锁单弹窗
     select 
         to_char(from_unixtime(time-25200),'yyyymmdd') special_date
         ,user_id
@@ -36,6 +47,16 @@ left join (
     group by 1,2,3
 ) suo 
 on suo.user_id = pay.user_id and suo.order_sn = pay.order_sn and suo.special_date = pay.special_date
-where suo.user_id is not null and suo.order_sn is not null and suo.special_date is not null
-group by 1,2,3,4
+left join (
+    -- 客服入口
+    select 
+        to_char(from_unixtime(time-25200),'yyyymmdd') special_date
+        ,user_id
+    from yishou_data.dcl_event_qiyu_click_d
+    where dt between '20240826' and '20250427'
+    and to_char(from_unixtime(time-25200),'yyyymmdd') between '20240826' and '20250426'
+    group by 1,2
+) qiyu 
+on suo.user_id = qiyu.user_id and suo.special_date = qiyu.special_date
+group by 1,2,3,4,5,6
 ;
